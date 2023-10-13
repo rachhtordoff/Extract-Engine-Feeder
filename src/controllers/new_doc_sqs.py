f4e11fcfd7be6f932aaf093d2137be89693752da
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-10-13 08:06:16 +0100

    amended get doc url

--- src/controllers/new_doc_sqs.py
+++ src/controllers/new_doc_sqs.py
@@ -1,63 +1,93 @@
 import json
 import os
-from src.utils.web_scrape import WebScraper
-from src.dependencies.openapi import DataExtractor
+import zipfile
+
+from src import config
 from src.utils import csv_generation
+from src.utils.web_scrape import WebScraper
+from src.utils.pdf_reader import PDFReader
+from src.dependencies.openapi import DataExtractor
 from src.dependencies.users_api import UserApi
 from src.utils.aws_s3 import AWSService
-from src.utils.pdf_reader import PDFReader
-from src import config
 
 
-class NewDocSqs(object):
+class NewDocSqs:
     def __init__(self, body):
+        self.body = body
+
         if body['type'] == 'urls':
-            scraped_websites = WebScraper().site_scrape(body['url_list'])
+            self.process_urls()
+
+        elif body['type'] == 'file':
+            self.process_files()
+
+    def process_urls(self):
+        scraped_websites = WebScraper().site_scrape(self.body['url_list'])
+        new_dict = {
+            "scraped_websites": scraped_websites,
+            "phrases_list": self.body.get('phrases_list')
+        }
+        extracted_data = DataExtractor().extract_data_from_webscraped_urls(new_dict)
+        self.output_data(extracted_data)
+
+    def process_files(self):
+        AWSService().download_file(self.body['id'], self.body['filename'])
+        _, file_extension = os.path.splitext(self.body['filename'])
+
+        if file_extension.lower() == '.pdf':
+            extracted_data = self.process_pdf()
+        elif file_extension.lower() == '.zip':
+            extracted_data = self.process_zip()
+        elif file_extension.lower() == '.csv':
+            extracted_data = self.process_csv()
+        self.output_data(extracted_data)
+        os.remove(f"{config.doc_location}{self.body['filename']}")
+
+    def process_csv(self):
+        extracted_list = []
+        csv_read = CSVReader().read_csv(self.body['filename'])
+        
+        for i in range(0, len(csv_read), 20):
+            batch_urls = csv_read[i:i+20]
+            scraped_websites = [WebScraper().site_scrape(url) for url in batch_urls]
             new_dict = {
                 "scraped_websites": scraped_websites,
-                "phrases_list": body.get('phrases_list')
+                "phrases_list": self.body.get('phrases_list')
             }
             extracted_data = DataExtractor().extract_data_from_webscraped_urls(new_dict)
-            if body.get('output_typeurl'):
-                if body['output_typeurl'] == 'CSV':
-                    file_path = csv_generation.create_csv(extracted_data, 'urls')
-                    UserApi(body).post_document(file_path, body['id'])
+            extracted_list.extend(extracted_data)
+        
+        self.output_data(extracted_list)
 
-                    data = {
-                        'extracted_data': extracted_data,
-                        'output_document_name': os.path.basename(file_path)
-                    }
+    def process_pdf(self):
+        pdf_read = PDFReader().read_pdf(self.body['filename'])
+        new_data = {
+            "phrases_list": self.body.get('phrases_list'),
+            "pdf_data": {self.body['filename']: pdf_read}
+        }
+        return DataExtractor().extract_data_from_pdf(new_data)
 
-                    UserApi(body).update_extraction(body['id'], data)
+    def process_zip(self):
+        with zipfile.ZipFile(self.body['filename'], 'r') as zip_ref:
+            zip_ref.extractall(f'{config.doc_location}/{self.body["filename"]}')
 
-                    os.remove(file_path)
-                    
-        elif body['type'] == 'file':
-            
-            AWSService().download_file(body['id'], body['filename'])
-            _, file_extension = os.path.splitext(body['filename'])
+        for foldername, subfolders, filenames in os.walk(f'{config.doc_location}/{self.body["filename"]}'):
+            for filename in filenames:
+                if filename.lower().endswith('.pdf'):
+                    return self.process_pdf()
+                elif filename.lower().endswith('.csv'):
+                    return self.process_csv()
 
-            if file_extension.lower() == '.pdf':
-                pdf_read = PDFReader().read_pdf(body['filename'])
 
-                new_data = {
-                    "phrases_list": body.get('phrases_list'),
-                    "pdf_data": {body['filename']: pdf_read}
-                }
-                extracted_data = DataExtractor().extract_data_from_pdf(new_data)
+    def output_data(self, extracted_data):
+        if self.body.get('output_typeurl') == 'CSV':
+            file_path = csv_generation.create_csv(extracted_data, 'urls')
+            UserApi(self.body).post_document(file_path, self.body['id'])
 
-            if body.get('output_typeurl'):
-                if body['output_typeurl'] == 'CSV':
-                    file_path = csv_generation.create_csv(extracted_data, 'urls')
-                    UserApi(body).post_document(file_path, body['id'])
+            data = {
+                'extracted_data': extracted_data,
+                'output_document_name': os.path.basename(file_path)
+            }
 
-                    data = {
-                        'extracted_data': extracted_data,
-                        'output_document_name': os.path.basename(file_path)
-                    }
-
-                    UserApi(body).update_extraction(body['id'], data)
-
-                    os.remove(file_path)
-
-            os.remove(f"{config.doc_location}{body['filename']}")
+            UserApi(self.body).update_extraction(self.body['id'], data)
+            os.remove(file_path)
--- src/utils/csv_generation.py
+++ src/utils/csv_generation.py
@@ -36,3 +36,23 @@ def create_csv(data, doc_type):
         writer.writerows(rows)
     
     return filename
+
+class CSVReader:
+    def __init__(self):
+        pass
+
+    def read_csv(self, filename):
+        try:
+            with pdfplumber.open(f"{config.doc_location}{filename}") as pdf:
+                # Concatenating text contents of all pages into a single string.
+                full_text = "\n".join([page.extract_text() for page in pdf.pages])
+                return full_text
+        except FileNotFoundError:
+            print(f"File not found: {filename}")
+            return None
+        except PermissionError:
+            print(f"No permission to read the file: {filename}")
+            return None
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
